9d90de5215e3a7f882226d64796eb6ecdf56482e
Александр <dominatorswen@mail.ru>
2016-09-08 17:14:52 +0300

    try play with db

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import {Db} from '../../db/db-component'
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
+
+
+
   constructor(public navCtrl: NavController) {
 
   }
+
+
 }
